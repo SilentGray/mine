3bf3bee53b42a470971076126411b1bbe2e63a80
Tom Lee <littleautomaton@gmail.com>
2013-12-23 23:42:08 +0000

    Fixups for the colours.
    Default to no colour usage, but make available 8 and 256-bit colors to
    play with.

--- display/interface.py
+++ display/interface.py
@@ -16,29 +16,24 @@ import logging as log
 # Module imports.
 from utils.exceptions import InterfaceException
 
+
 class InvalidColours(InterfaceException):
     pass
 
 INVALID_COLOUR_VALUE = 'Attempted to set colour %d, only %d available.'
 
 # What colours to use for what.
-# Use 256-colours and ANSI escape codes.
-BACK_NORM=234
-BACK_HIGHLIGHT=244
-FORE_NORM=251
-FORE_SUBDUED=242
-FORE_HIGHLIGHT=255
+#
+# Set the appropriate colour function.  This should select the appropriate
+# scheme in the future, but for now hardcode the choice here.
+clrs = 0
 
-# What symbols to use for what.
-PROMPT = '>>  '
-EDGE = '|'
-SEPERATOR = '-'
-BLANK = ' '
 
-# Colour application.
-def bold():
-    """Sets the font to be bold."""
-    return('\033[1m')
+def colour_0(value, back):
+    """Dummy function for callbacks that attempt to set colour with no
+    colours in use."""
+    return('')
+
 
 def colour_8(value, back):
     """Sets a 8-colour value.  Sets text colour, or background colour."""
@@ -50,6 +45,7 @@ def colour_8(value, back):
         key = 3
     return('\033[%d%dm' % (key, value))
 
+
 def colour_256(value, back):
     """Sets a 256-colour value.  Sets text colour, or background colour."""
     if value >= 256:
@@ -60,15 +56,52 @@ def colour_256(value, back):
         key = 38
     return('\033[%d;05;%dm' % (key, value))
 
+if clrs == 256:
+    colour = colour_256
+    BACK_NORM = 234
+    BACK_HIGHLIGHT = 244
+    FORE_NORM = 251
+    FORE_SUBDUED = 242
+    FORE_HIGHLIGHT = 255
+elif clrs == 8:
+    colour = colour_8
+    BACK_NORM = 0
+    BACK_HIGHLIGHT = 4
+    FORE_NORM = 6
+    FORE_SUBDUED = 2
+    FORE_HIGHLIGHT = 7
+else:
+    colour = colour_0
+    BACK_NORM = 0
+    BACK_HIGHLIGHT = 0
+    FORE_NORM = 0
+    FORE_SUBDUED = 0
+    FORE_HIGHLIGHT = 0
+
+# What symbols to use for what.
+PROMPT = '>>  '
+EDGE = '|'
+SEPERATOR = '-'
+BLANK = ' '
+
+
+# Colour application.
+def bold():
+    """Sets the font to be bold."""
+    return('\033[1m')
+
+
 def applyColour(value, back=False):
     """Sets a colour, accomodating for colours available."""
     log.debug('Apply colour: %d' % value)
-    return(colour_256(value, back))
+    return(colour(value, back))
+
 
 def applyColours(fore_col, back_col):
     """Sets both fore and background colours."""
     log.debug('Apply colours: Fore=%d, Back=%d' % (fore_col, back_col))
     return(applyColour(fore_col, False) + applyColour(back_col, True))
+
 
 def hightlitColours(fore_col=FORE_HIGHLIGHT, back_col=BACK_NORM):
     """Sets text and background colours and applies a bold font."""
@@ -76,15 +109,18 @@ def hightlitColours(fore_col=FORE_HIGHLIGHT, back_col=BACK_NORM):
               (fore_col, back_col))
     return(bold() + applyColours(fore_col, back_col))
 
+
 def subduedColours():
     """Resets the text and background colours to subdued."""
     log.debug('Apply colours: Set subdued colours.')
     return(applyColours(FORE_SUBDUED, BACK_NORM))
 
+
 def resetColours():
     """Resets the text and background colours to default."""
     log.debug('Apply colours: Set default formatting.')
     return(applyColours(FORE_NORM, BACK_NORM))
+
 
 def cleanColours():
     """Removes all formatting."""
@@ -95,6 +131,7 @@ def cleanColours():
 LINE_START = (subduedColours() + EDGE + resetColours())
 LINE_END = (subduedColours() + EDGE)
 
+
 def printLine(line, padding=True):
     """Prints the line and handles edge formatting."""
     if padding:
@@ -102,24 +139,29 @@ def printLine(line, padding=True):
     else:
         print(LINE_START + line + LINE_END)
 
+
 def printRefresh():
     """Print to clear the screen and reset cursor."""
     print('\033[2J\033[h')
     return
+
 
 def printSpacer():
     """Prints a single spacer line"""
     printLine((subduedColours() + ('{:%s^78}' % SEPERATOR).format('')),
               padding=False)
 
+
 def printText(text):
     """Prints a single block of text"""
     for line in text.split('\n'):
         printLine(('{:%s<74}' % BLANK).format(line))
 
+
 def printBlank():
     """Prints a blank line"""
     printLine(('{:%s^74}' % BLANK).format(''))
+
 
 def printTwoColumns(text1, text2):
     """Prints two columns of text side-by-side"""
@@ -140,6 +182,7 @@ def printTwoColumns(text1, text2):
 
         printLine(('{:%s<35}' % BLANK).format(string1) + 4 * BLANK +
                   ('{:%s<35}' % BLANK).format(string2))
+
 
 def userInput(promptText, options):
     """Gets a users choice for an action"""
